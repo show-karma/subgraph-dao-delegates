b1bf11a6034a0f6cb5cfe0c69b160b0c54ffbace
Mahesh Murthy <mahesh@mmurthy.com>
2022-04-04 10:04:51 -0400

    uniswap and pooltogether

---
+++ src/uniswap_mapping.ts
@@ -0,0 +1,41 @@
+import { ipfs, json, BigInt, BigDecimal, Bytes, log } from "@graphprotocol/graph-ts"
+import {
+  Organization,
+  User,
+  DelegateOrganization,
+  DelegatorOrganization
+} from "../generated/schema"
+import { DelegateChanged, DelegateVotesChanged } from "../generated/UniswapToken/UniswapToken"
+
+export function delegateChanged(event: DelegateChanged): void {
+  let organization = new Organization("uniswap")
+  organization.token = "uni"
+  organization.save()
+
+  let delegate = new User(event.params.toDelegate.toHexString())
+  delegate.save();
+
+  let delegator = new User(event.params.delegator.toHexString())
+  delegator.save();
+
+  let delegatorOrganization = new DelegatorOrganization(`${delegator.id}-${organization.id}`)
+  delegatorOrganization.delegate = delegate.id
+  delegatorOrganization.delegator = delegator.id
+  delegatorOrganization.organization = organization.id
+  delegatorOrganization.save()
+}
+
+export function delegateVotesChanged(event: DelegateVotesChanged): void {
+  let organization = new Organization("uniswap")
+  organization.token = "uni"
+  organization.save()
+
+  let user = new User(event.params.delegate.toHexString())
+  user.save();
+
+  let delegateOrganization = new DelegateOrganization(`${user.id}-${organization.id}`)
+  delegateOrganization.delegate = user.id
+  delegateOrganization.organization = organization.id
+  delegateOrganization.voteBalance = event.params.newBalance
+  delegateOrganization.save()
+}
