1c3f8cee47462136baa2e882b6119f224ad7fb70
Danylo Shapoval <danylo.shapoval@softonix.org>
2022-04-21 13:48:21 +0300

    ampleforthorg

---
+++ src/ampleforth_mapping.ts
@@ -0,0 +1,43 @@
+import {
+  Organization,
+  User,
+  DelegateOrganization,
+  DelegatorOrganization
+} from "../generated/schema"
+import { DelegateChanged, DelegateVotesChanged } from "../generated/AmpleforthToken/AmpleforthToken"
+
+const dao = 'ampleforthorg.eth';
+const token = 'forth';
+
+export function delegateChanged(event: DelegateChanged): void {
+  let organization = new Organization(dao)
+  organization.token = token;
+  organization.save()
+
+  let delegate = new User(event.params.toDelegate.toHexString())
+  delegate.save();
+
+  let delegator = new User(event.params.delegator.toHexString())
+  delegator.save();
+
+  let delegatorOrganization = new DelegatorOrganization(`${delegator.id}-${organization.id}`)
+  delegatorOrganization.delegate = delegate.id
+  delegatorOrganization.delegator = delegator.id
+  delegatorOrganization.organization = organization.id
+  delegatorOrganization.save()
+}
+
+export function delegateVotesChanged(event: DelegateVotesChanged): void {
+  let organization = new Organization(dao)
+  organization.token = token;
+  organization.save()
+
+  let user = new User(event.params.delegate.toHexString())
+  user.save();
+
+  let delegateOrganization = new DelegateOrganization(`${user.id}-${organization.id}`)
+  delegateOrganization.delegate = user.id
+  delegateOrganization.organization = organization.id
+  delegateOrganization.voteBalance = event.params.newBalance
+  delegateOrganization.save()
+}
