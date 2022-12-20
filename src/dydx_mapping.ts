f6702154666d9acfd356726a366962a428d4a2f6
Arthur <32548365+Arthh@users.noreply.github.com>
2022-12-20 12:53:49 -0300

    feature: dydx integration

---
+++ src/dydx_mapping.ts
@@ -0,0 +1,63 @@
+import {
+  ipfs,
+  json,
+  BigInt,
+  BigDecimal,
+  Bytes,
+  log,
+} from "@graphprotocol/graph-ts";
+import {
+  Organization,
+  User,
+  DelegateOrganization,
+  DelegatorOrganization,
+} from "../generated/schema";
+import {
+  DelegateChanged,
+  DelegatedPowerChanged,
+} from "../generated/DYDXToken/DYDXToken";
+import { getDelegateOrganization } from "./shared/getDelegateOrganization";
+import { getFirstTokenDelegatedAt } from "./shared/getFirstTokenDelegatedAt";
+
+export function delegateChanged(event: DelegateChanged): void {
+  let organization = new Organization("dydx");
+  organization.token = "dydx";
+  organization.save();
+
+  let delegate = new User(event.params.delegatee.toHexString());
+  delegate.save();
+
+  let delegator = new User(event.params.delegator.toHexString());
+  delegator.save();
+
+  let delegatorOrganization = new DelegatorOrganization(
+    `${delegator.id}-${organization.id}`
+  );
+  delegatorOrganization.delegate = delegate.id;
+  delegatorOrganization.delegator = delegator.id;
+  delegatorOrganization.organization = organization.id;
+  delegatorOrganization.save();
+}
+
+export function delegateVotesChanged(event: DelegatedPowerChanged): void {
+  let organization = new Organization("dydx");
+  organization.token = "dydx";
+  organization.save();
+
+  let user = new User(event.params.user.toHexString());
+  user.save();
+
+  const delegateOrganizationId = `${user.id}-${organization.id}`;
+  const delegateOrganization = getDelegateOrganization(delegateOrganizationId);
+
+  delegateOrganization.delegate = user.id;
+  delegateOrganization.organization = organization.id;
+  delegateOrganization.voteBalance = event.params.amount;
+
+  delegateOrganization.firstTokenDelegatedAt = getFirstTokenDelegatedAt(
+    event,
+    delegateOrganization
+  );
+
+  delegateOrganization.save();
+}
