b2bfe94a487f70893818befff04e92f658aef44a
Arthur <32548365+Arthh@users.noreply.github.com>
2023-02-14 13:07:39 -0300

    fix: delegateHistory

--- src/feiprotocol_mapping.ts
+++ src/feiprotocol_mapping.ts
@@ -78,6 +78,30 @@ export function delegateVotesChanged(event: DelegateVotesChanged): void {
   
   delegateOrganization.save();
 
+  let delegatingHistory = DelegatingHistory.load(event.transaction.hash.toHexString())
+
+  if(!delegatingHistory){
+    delegatingHistory = new DelegatingHistory(event.transaction.hash.toHexString())
+    delegatingHistory.amount = BigInt.zero();
+    delegatingHistory.timestamp = event.block.timestamp;
+    delegatingHistory.delegator = "0";
+  }
+
+  let delegatedAmount = event.params.newBalance.minus(event.params.previousBalance)
+
+  if (delegatedAmount < BigInt.zero()) {
+    delegatingHistory.fromDelegate = user.id
+  } else {
+    delegatingHistory.toDelegate = user.id
+  }
+  if (delegatingHistory.amount) {
+    delegatingHistory.amount = delegatingHistory.amount.plus(delegatedAmount);
+  } else {
+    delegatingHistory.amount = delegatedAmount;
+  }
+
+  delegatingHistory.save();
+
   const delegatePowerChange = new DelegateVotingPowerChange(
     event.transaction.hash.toHexString()
   );
